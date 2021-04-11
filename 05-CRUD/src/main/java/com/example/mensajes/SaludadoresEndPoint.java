c69844efa29e508af264c3f09f082a1effe5cc7f
JohnnyBonilla12 <71783309+JohnnyBonilla12@users.noreply.github.com>
2021-04-11 15:32:17 -0500

    Success

---
+++ 05-CRUD/src/main/java/com/example/mensajes/SaludadoresEndPoint.java
@@ -0,0 +1,88 @@
+package com.example.mensajes;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import me.tell.mensajes.CreateRequest;
+import me.tell.mensajes.CreateResponse;
+import me.tell.mensajes.DeleteRequest;
+import me.tell.mensajes.DeleteResponse;
+import me.tell.mensajes.ReadAllRequest;
+import me.tell.mensajes.ReadAllResponse;
+import me.tell.mensajes.ReadRequest;
+import me.tell.mensajes.ReadResponse;
+import me.tell.mensajes.UpdateRequest;
+import me.tell.mensajes.UpdateResponse;
+
+
+@Endpoint
+public class SaludadoresEndPoint {
+    @Autowired
+    private Isaludadores isaludadores;
+
+    @PayloadRoot(namespace = "http://tell.me/mensajes", localPart = "CreateRequest")
+    @ResponsePayload
+    public CreateResponse crearUsuario(@RequestPayload CreateRequest peticion){
+        CreateResponse respuesta = new CreateResponse();
+        respuesta.setRespuesta("Hola, " + peticion.getNombre());
+        Saludadores saludadores = new Saludadores();
+        saludadores.setNombre(peticion.getNombre());
+        isaludadores.save(saludadores);
+        return respuesta;
+    }
+
+    @PayloadRoot(namespace = "http://tell.me/mensajes", localPart = "ReadAllRequest")
+    @ResponsePayload
+    public ReadAllResponse leerUsuarios(@RequestPayload ReadAllRequest peticion){
+        ReadAllResponse respuesta = new ReadAllResponse();
+        Iterable<Saludadores> listaSaludadores = isaludadores.findAll();
+
+        for(Saludadores ls : listaSaludadores){
+            ReadAllResponse.Saludador e = new ReadAllResponse.Saludador();
+            e.setId(ls.getId());
+            e.setNombre(ls.getNombre());
+            respuesta.getSaludador().add(e);
+        }
+
+        return respuesta;
+    }
+
+    @PayloadRoot(namespace = "http://tell.me/mensajes", localPart = "ReadRequest")
+    @ResponsePayload
+    public ReadResponse obtenerUsuario(@RequestPayload ReadRequest peticion){
+        ReadResponse respuesta = new ReadResponse();
+        Optional<Saludadores> local;
+        local = isaludadores.findById(peticion.getId());
+        respuesta.setId(local.get().getId());
+        respuesta.setNombre(local.get().getNombre());
+        return respuesta;
+    }
+
+    @PayloadRoot(namespace = "http://tell.me/mensajes", localPart = "UpdateRequest")
+    @ResponsePayload
+    public UpdateResponse editarUsuario(@RequestPayload UpdateRequest peticion){
+        UpdateResponse respuesta = new UpdateResponse();
+        Optional<Saludadores> local;
+        local = isaludadores.findById(peticion.getId());
+        local.get().setNombre(peticion.getNombre());
+        isaludadores.save(local.get());
+        respuesta.setRespuesta("Usuario Modificado Correctamente " + local.get().toString().trim());
+        return respuesta;
+    }
+
+    @PayloadRoot(namespace = "http://tell.me/mensajes", localPart = "DeleteRequest")
+    @ResponsePayload
+    public DeleteResponse borrarUsuario(@RequestPayload DeleteRequest peticion){
+        DeleteResponse respuesta = new DeleteResponse();
+        Optional<Saludadores> local;
+        local = isaludadores.findById(peticion.getId());
+        isaludadores.deleteById(peticion.getId());
+        respuesta.setRespuesta("Usuario Borrado Correctamente " + local.get().toString().trim());
+        return respuesta;
+    }
+}
---
+++ 05-CRUD/src/main/java/me/tell/mensajes/ObjectFactory.java
@@ -0,0 +1,127 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.04.11 a las 03:12:29 PM CDT 
+//
+
+
+package me.tell.mensajes;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the me.tell.mensajes package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: me.tell.mensajes
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link ReadAllResponse }
+     * 
+     */
+    public ReadAllResponse createReadAllResponse() {
+        return new ReadAllResponse();
+    }
+
+    /**
+     * Create an instance of {@link UpdateResponse }
+     * 
+     */
+    public UpdateResponse createUpdateResponse() {
+        return new UpdateResponse();
+    }
+
+    /**
+     * Create an instance of {@link DeleteResponse }
+     * 
+     */
+    public DeleteResponse createDeleteResponse() {
+        return new DeleteResponse();
+    }
+
+    /**
+     * Create an instance of {@link ReadResponse }
+     * 
+     */
+    public ReadResponse createReadResponse() {
+        return new ReadResponse();
+    }
+
+    /**
+     * Create an instance of {@link UpdateRequest }
+     * 
+     */
+    public UpdateRequest createUpdateRequest() {
+        return new UpdateRequest();
+    }
+
+    /**
+     * Create an instance of {@link CreateRequest }
+     * 
+     */
+    public CreateRequest createCreateRequest() {
+        return new CreateRequest();
+    }
+
+    /**
+     * Create an instance of {@link CreateResponse }
+     * 
+     */
+    public CreateResponse createCreateResponse() {
+        return new CreateResponse();
+    }
+
+    /**
+     * Create an instance of {@link ReadAllRequest }
+     * 
+     */
+    public ReadAllRequest createReadAllRequest() {
+        return new ReadAllRequest();
+    }
+
+    /**
+     * Create an instance of {@link ReadAllResponse.Saludador }
+     * 
+     */
+    public ReadAllResponse.Saludador createReadAllResponseSaludador() {
+        return new ReadAllResponse.Saludador();
+    }
+
+    /**
+     * Create an instance of {@link DeleteRequest }
+     * 
+     */
+    public DeleteRequest createDeleteRequest() {
+        return new DeleteRequest();
+    }
+
+    /**
+     * Create an instance of {@link ReadRequest }
+     * 
+     */
+    public ReadRequest createReadRequest() {
+        return new ReadRequest();
+    }
+
+}
